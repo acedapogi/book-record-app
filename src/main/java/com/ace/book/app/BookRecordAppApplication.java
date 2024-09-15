0b100aa6849026224f6445532719487464b6a417
ACE <acedapogi@yahoo.com>
2024-09-15 20:45:52 +0800

    Initial commit

---
+++ src/main/java/com/ace/book/app/BookRecordAppApplication.java
@@ -0,0 +1,13 @@
+package com.ace.book.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecordAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace/book/app/BookRecordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.book.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecordAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
